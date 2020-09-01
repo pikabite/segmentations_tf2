876bd1ff8271e281f2fe7fa47b78e911c44fa92e
daeyounkim <daeyoun.kim@dabeeo.com>
2020-09-01 11:46:24 +0900

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Tensorflow 2.1 Segmentation pipeline implementation from scratch
 
+---
+
+## Process with Cityscape datasets
+### Cityscape data with HRNet
+
+cityscape dataset (https://www.cityscapes-dataset.com/)  
+HRNet (https://arxiv.org/pdf/1908.07919.pdf)  
+
+HRNetV2W48 model  
+mIOU 70.31% (without pre-trained backbone)  
+  
+| Input image | Ground truth | Segmentation result |  
+![cityscape hrnet result 1](./outputs/cityscape_hrnet/frankfurt_000001_079206_leftImg8bit.png)  
+![cityscape hrnet result 2](./outputs/cityscape_hrnet/frankfurt_000001_044658_leftImg8bit.png)  
+![cityscape hrnet result 3](./outputs/cityscape_hrnet/munster_000001_000019_leftImg8bit.png)  
+![cityscape hrnet result 3](./outputs/cityscape_hrnet/munster_000051_000019_leftImg8bit.png)  
+
+
 
 ---   
 
--- utils/util.py
+++ utils/util.py
@@ -2,50 +2,20 @@
 import numpy as np
 from PIL import Image
 
-import tensorflow as tf
-from tensorflow import keras as tk
 
 #%%
 
-def preprocessing (image, mask, options) :
+def label_to_color (imgs, colormap) :
 
-    if "hflip" in options and options["hflip"] :
-        if np.random.randint(0, 2) == 1 :
-            image = image[::-1, :, :]
-            mask = mask[::-1, :, :]
-    if "vflip" in options and options["vflip"] :
-        if np.random.randint(0, 2) == 1 :
-            image = image[:, ::-1, :]
-            mask = mask[:, ::-1, :]
-    if "brightness_range" in options and options["brightness_range"] is not None :
-        assert len(options["brightness_range"]) == 2, "brightness should be formed [min, max]"
-        b_number = np.random.uniform(options["brightness_range"][0], options["brightness_range"][1])
-        image = image*b_number
-    if "scale" in options and options["scale"] :
-        image /= 255
-    if "noise" in options and options["noise"] :
-        # print("noise ahn hae tta")
-        pass
+    newimg = np.zeros((imgs.shape[0], imgs.shape[1], imgs.shape[2]))
+    imgs = np.mean(imgs, axis=-1)
 
-    # print(np.sum((mask == 255)*1))
-
-    return image, mask
+    for i in range(len(colormap)) :
+        newimg[imgs == i] = colormap[i]
+    
+    return newimg.astype(np.uint8)
 
 
-#%%
-
-def calculate_mask_iou (masks1, masks2, class_number) :
-    # tmp1 = (mask1 == class_number)*1
-    # tmp2 = (mask2 == class_number)*1
-
-    mask_sum = masks1 + masks2
-    i = np.sum((mask_sum == class_number*2)*1, axis=(1, 2, 3))
-    u = np.sum((mask_sum >= class_number)*1, axis=(1, 2, 3))
-
-    return np.divide(i, u, where=u!=0)
-    # return np.clip(i / u, 0, 1.0)
-
-
-
-#%%
-
+# %%
+def unnorm (imgs, mean, std) :
+    return (((imgs * std) + mean)*255).astype(np.uint8)

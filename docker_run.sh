1a5e3c9c1a39195d5692d1984ab639848b7e2629
pikabite <kdy9007@gmail.com>
2020-10-26 11:32:04 +0900

    docker file, merge code

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 DATASET_PATH="/media"
-docker run --runtime=nvidia -v ${PWD}:/dy -v $DATASET_PATH:/datasets  -d  -it --name dy_seg_tf2 -p 15016:15016 seg_tf2:1
+docker run --gpus=all -v ${PWD}:/dy -v $DATASET_PATH:/datasets  -d  -it --name dy_seg_tf2 -p 15016:15016 seg_tf2:1
 
